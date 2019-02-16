08c4934126d7902ec0794fc0ecd2d2f00ba08fa5
RitvikKhanna <ritvik@ualberta.ca>
2019-02-16 15:21:56 -0700

    First change
    Added main code and first main activity
    Coding team add necessary code to start signIn acitvity. I will develop UI for the same

--- code/app/src/main/java/com/example/ibookit/SignIn.java
+++ code/app/src/main/java/com/example/ibookit/SignIn.java
@@ -2,15 +2,12 @@ package com.example.ibookit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_sign_in);
     }
 }
