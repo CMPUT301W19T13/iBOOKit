698f3249599fc3a3ef07a9407b49003246e2e623
RitvikKhanna <ritvik@ualberta.ca>
2019-02-17 16:27:35 -0700

    added signIn page

--- code/app/src/main/java/com/example/ibookit/MainActivity.java
+++ code/app/src/main/java/com/example/ibookit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ibookit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button check = (Button) findViewById(R.id.logInButton);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
--- code/app/src/main/java/com/example/ibookit/SignIn.java
+++ code/app/src/main/java/com/example/ibookit/SignIn.java
@@ -1,7 +1,10 @@
 package com.example.ibookit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button check = (Button) findViewById(R.id.logInButton);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
