e045ff5672c7797e15792d2428aec3593493a34b
joe <jlx@ualberta.ca>
2019-02-19 15:08:45 -0700

    Create Requests.java

---
+++ code/app/src/test/java/com/example/ibookit/Requests.java
@@ -0,0 +1,24 @@
+package com.example.ibookit;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class Requests {
+
+
+    // send request to user 1 for a specific book
+
+    @Test
+    public void SendRequest(){
+        //
+    }
+
+
+
+
+
+}
