a300d05d3623b66deb5258101c0297b702623493
chongzhe <chongzhe@ualberta.ca>
2019-02-19 14:05:04 -0800

    UPDATE SPINNER

--- code/app/src/main/java/com/example/ibookit/Book.java
+++ code/app/src/main/java/com/example/ibookit/Book.java
@@ -3,11 +3,21 @@ package com.example.ibookit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
     }
+    MultiSelectionSpinner spinner=(MultiSelectionSpinner)findViewById(R.id.input1);
+
+    List<String> list = new ArrayList<String>();
+          list.add("List1");
+          list.add("List2");
+          spinner.setItems(list);
 }
