9784cdb31778cb4135314a0b2d9f15fa3454c886
joe <jlx@ualberta.ca>
2019-02-19 14:30:25 -0700

    Create ObjectLoad.java

---
+++ code/app/src/test/java/com/example/ibookit/ObjectLoad.java
@@ -0,0 +1,58 @@
+package com.example.ibookit;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.security.acl.Owner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ObjectLoad {
+
+    // Book book = new Book( .......)
+
+    @Test
+    public void ReturnBookinfo(){
+
+        // see if the book object can return all of its correct states
+
+    }
+
+    // User user = new User (...)
+    @Test
+    public void ReturnUserInfo(){
+        // see if the user can returns all of its correct states
+    }
+    // OwnerShelf Shelf = new Shelf(...)
+    // BorrowerShelf BShelf = new BShelf(...)
+    @Test
+    public void ReturnShelf() {
+        // see if user and owner shelfs can return its states
+
+        }
+
+
+    // Scan returns Strings of name isbn
+    @Test
+    public void Scan(String name, String ISBN){
+        //// test to see if the scanned name and ISBN correctly
+    }
+
+    // get array list of users and books in the database
+    @Test
+    public void Search(){
+        /// search for books and users, see if the books and users returned matches the ones in
+        // the database
+    }
+
+
+
+
+
+
+}
