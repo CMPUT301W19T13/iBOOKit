da8658e5021e1dfb2f178e526e6c33fe118c1b41
zijunualberta <zijun4@ualbertaclear.ca>
2019-02-19 23:14:09 -0700

    create model package

---
+++ code/app/src/main/java/com/example/ibookit/Model/Book.java
@@ -0,0 +1,69 @@
+package com.example.ibookit.Model;
+
+import com.example.ibookit.Model.User;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String category;
+    private Integer status;
+    private User owner;
+    private User borrower;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+}
