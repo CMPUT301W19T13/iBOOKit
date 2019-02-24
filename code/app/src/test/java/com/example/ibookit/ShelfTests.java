c585cce7b701e7b0906011a01cd28eff485e70f4
joe <jlx@ualberta.ca>
2019-02-24 13:31:36 -0700

    Create ShelfTests.java

---
+++ code/app/src/test/java/com/example/ibookit/ShelfTests.java
@@ -0,0 +1,39 @@
+package com.example.ibookit;
+
+import com.example.ibookit.Model.Book;
+import com.example.ibookit.Model.OwnerShelf;
+import com.example.ibookit.Model.User;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ShelfTests {
+
+     private ArrayList<Book> myBooks;
+     OwnerShelf owner = new OwnerShelf();
+
+     @Test
+     public void OwnerShelf(){
+
+         myBooks = owner.All_books();
+
+
+
+     }
+
+
+
+
+
+
+
+
+}
