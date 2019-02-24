e45bce04a3a0656b46f2b9ef47eb92726a5d365f
joe <jlx@ualberta.ca>
2019-02-24 14:15:23 -0700

    Create SearchUser.java

---
+++ code/app/src/main/java/com/example/ibookit/Functionality/SearchUser.java
@@ -0,0 +1,23 @@
+package com.example.ibookit.Functionality;
+
+import com.example.ibookit.Model.User;
+
+import java.util.ArrayList;
+
+public class SearchUser {
+
+
+    // returns all possible users that match the searches string
+
+    private ArrayList<User> allUsers;
+
+    public ArrayList<User> getUserId(String searchString){
+
+
+
+
+        return allUsers;
+    }
+
+
+}
