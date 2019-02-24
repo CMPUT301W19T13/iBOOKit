e4d63864726c0cfa8dcfd2eb03281b08250e71ed
joe <jlx@ualberta.ca>
2019-02-24 13:31:37 -0700

    Create BorrowerShelf.java

---
+++ code/app/src/main/java/com/example/ibookit/Model/BorrowerShelf.java
@@ -0,0 +1,13 @@
+package com.example.ibookit.Model;
+
+import java.util.ArrayList;
+
+public class BorrowerShelf {
+    private ArrayList<Book> BorrowBooks;
+
+        public void All_books(){
+            //returns all books that you are borrowing
+        }
+
+
+}
