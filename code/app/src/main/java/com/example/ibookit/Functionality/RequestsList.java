5cc6b0ad741ad4b144f0f066fa6eec396f4b11bc
joe <jlx@ualberta.ca>
2019-02-25 12:33:55 -0700

    Create RequestsList.java

---
+++ code/app/src/main/java/com/example/ibookit/Functionality/RequestsList.java
@@ -0,0 +1,46 @@
+package com.example.ibookit.Functionality;
+
+import com.example.ibookit.Model.Book;
+import com.example.ibookit.Model.User;
+
+import java.util.ArrayList;
+
+public class RequestsList {
+
+
+    private Book book;
+    private ArrayList<User> BorrowUsers;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public ArrayList<User> getBorrowUsers() {
+        return BorrowUsers;
+    }
+
+    public void setBorrowUsers(ArrayList<User> borrowUsers) {
+        BorrowUsers = borrowUsers;
+    }
+
+
+
+    public void ShowBook(){};
+    public void ShowRequester(){};
+    public void AcceptRequest(){};
+    public void DeclineReqeust(){};
+
+
+
+
+
+
+
+
+
+
+}
