e3ddb38990c712e0af4c8b936d9dcba3a90a2c00
jason <jiazhen2@ualberta.ca>
2019-03-06 16:52:09 -0700

    requestsent basic

--- code/app/src/main/java/com/example/ibookit/Model/RequestSent.java
+++ code/app/src/main/java/com/example/ibookit/Model/RequestSent.java
@@ -1,10 +1,55 @@
 package com.example.ibookit.Model;
 
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
 import com.example.ibookit.Functionality.Request;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class RequestSent {
-    private ArrayList<Request> requestSent;
-    private User user;
+    private static final String TAG = "RequestSent";
+    private ArrayList<Request> requestSent = new ArrayList<>();
+    private DatabaseReference mDatabase;
+    private DatabaseReference BorrowId;
+    private String username;
+
+
+
+    public RequestSent(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        username = user.getDisplayName();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(username).child("RequestSent");
+    }
+
+    public void SyncRequestSent(final ArrayList<Request> requests, final ArrayAdapter<Request> adapter) {
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                requests.clear();
+                adapter.notifyDataSetChanged();
+                for (DataSnapshot d: dataSnapshot.getChildren()) {
+                    Request request = d.getValue(Request.class);
+                    requests.add(request);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: ");
+            }
+        });
+
+    }
+    public void RetriveRequest(){
+    BorrowId = FirebaseDatabase.getInstance().getReference().child("users").child(username).child("Requests");
+    }
+
 }
