709a54932923719038c1824407942fafc3c0f9ea
joe <jlx@ualberta.ca>
2019-03-07 15:33:38 -0700

    Update MainActivity.java

--- code/app/src/main/java/com/example/ibookit/View/MainActivity.java
+++ code/app/src/main/java/com/example/ibookit/View/MainActivity.java
@@ -1,39 +1,123 @@
 package com.example.ibookit.View;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.ibookit.Functionality.RequestsList;
+import com.example.ibookit.Model.Request;
 import com.example.ibookit.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText mEmail, mPassword;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button logInButton = (Button) findViewById(R.id.main_logIn);
+
+        Button logInButton = (Button) findViewById(R.id.logInButton);
 
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignIn.class);
-                startActivity(intent);
+                mEmail = findViewById(R.id.emailEnter);
+                mPassword = findViewById(R.id.password_enter);
+
+                Log.d(TAG, "onClick: " + mEmail.getText().toString());
+                Log.d(TAG, "onClick: " + mPassword.getText().toString());
+
+
+                if ((!mEmail.getText().toString().isEmpty()) && (!mPassword.getText().toString().isEmpty())) {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Toast.makeText(MainActivity.this, "Login successful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Login fail",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    Log.d(TAG, "onClick: Empty");
+                    Toast.makeText(MainActivity.this, "Cannot leave empty",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        Button signUpButton = (Button) findViewById(R.id.main_signUp);
+        setupFirebaseAuth();
+
+
+        Button signUpButton = (Button) findViewById(R.id.signUp_Main);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
     }
-    
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void setupFirebaseAuth() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "setupFirebaseAuth: Success");
+                    Intent intent = new Intent(MainActivity.this, HomeSearchActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG, "setupFirebaseAuth: Fail");
+                }
+            }
+        };
+
+    }
+
+
+
+
 }
