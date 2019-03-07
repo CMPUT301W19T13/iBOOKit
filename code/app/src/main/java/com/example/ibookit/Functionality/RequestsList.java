a80dddb4546957316c82a8d68dedffb6e3b2733d
joe <jlx@ualberta.ca>
2019-03-06 23:05:30 -0700

    Update RequestsList.java

--- code/app/src/main/java/com/example/ibookit/Functionality/RequestsList.java
+++ code/app/src/main/java/com/example/ibookit/Functionality/RequestsList.java
@@ -1,8 +1,16 @@
 package com.example.ibookit.Functionality;
 
+import android.support.annotation.NonNull;
+
 import com.example.ibookit.Model.Book;
 import com.example.ibookit.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class RequestsList {
@@ -10,6 +18,10 @@ public class RequestsList {
 
     private Book book;
     private ArrayList<User> BorrowUsers;
+    private DatabaseReference mDatabase;
+
+
+    
 
     public Book getBook() {
         return book;
