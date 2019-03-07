114c8006dd050296053f5d8956772984286c5e71
joe <jlx@ualberta.ca>
2019-03-07 15:36:23 -0700

    Update Search.java

--- code/app/src/main/java/com/example/ibookit/Functionality/Search.java
+++ code/app/src/main/java/com/example/ibookit/Functionality/Search.java
@@ -1,31 +1,17 @@
 package com.example.ibookit.Functionality;
 
+import android.widget.ArrayAdapter;
+
 import com.example.ibookit.Model.Book;
 import com.example.ibookit.Model.User;
 
 import java.util.ArrayList;
 
-public class Search {
+public interface Search {
 
+//    void searchByKeyword(final String mKeyword, final ArrayList<Book> result, final ArrayAdapter<Book> adapter);
+//    ArrayList searchByName();
 
-    // returns all possible users that match the searches string
-
-    private ArrayList<User> allUsers;
-    private ArrayList<Book> allBooks;
-
-    public ArrayList<User> getUserId(String searchString){
-
-
-
-        return allUsers;
-    }
-
-    public ArrayList<Book> getBooks(String searchString){
-
-
-
-        return allBooks;
-    }
 
 
 }
