26194a0d5dc369baad7401108a3551ab2c7cc480
zisen01 <zisen@ualberta.ca>
2019-03-11 14:21:13 -0600

    some local tests added

---
+++ code/app/src/androidTest/java/com/example/ibookit/BorrowerShelfTest.java
@@ -0,0 +1,34 @@
+package com.example.ibookit;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.ibookit.Model.Book;
+import com.example.ibookit.Model.BorrowerShelf;
+import com.example.ibookit.Model.OwnerShelf;
+import com.google.firebase.FirebaseApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class BorrowerShelfTest {
+    private Context instrumentationCtx;
+    private BorrowerShelf testShelf = new BorrowerShelf();
+
+    @Test
+    public void test(){
+        instrumentationCtx = InstrumentationRegistry.getContext();
+        FirebaseApp.initializeApp(instrumentationCtx);
+
+        //test All_Books method in BorrowerShelf
+        ArrayList<Book> expected= new ArrayList<>();
+        assertEquals(expected, testShelf.All_books());
+
+    }
+}
