0248d2824f16ceac25b4cb50ecfe1dddc8459dc2
zijunualberta <zijun4@ualbertaclear.ca>
2019-03-20 18:17:30 -0600

    new recommendation

--- code/app/src/main/java/com/example/ibookit/Model/Recommendation.java
+++ code/app/src/main/java/com/example/ibookit/Model/Recommendation.java
@@ -9,6 +9,8 @@
  */
 package com.example.ibookit.Model;
 
+import java.util.HashMap;
+
 /**
  * @author zijun wu
  *
@@ -16,202 +18,32 @@ package com.example.ibookit.Model;
  */
 public class Recommendation {
 
-    private String username;
-    private double Classics;
-    private Integer ClassicsCount;
-    private double SciFi;
-    private Integer SciFiCount;
-    private double Horror;
-    private Integer HorrorCount;
-    private double Thriller;
-    private Integer ThrillerCount;
-    private double Sports;
-    private Integer SportsCount;
-    private double Comics;
-    private Integer ComicsCount;
-    private double Romance;
-    private Integer RomanceCount;
-    private double Business;
-    private Integer BusinessCount;
-    private double other;
-    private Integer otherCount;
+    private HashMap<String, Double> category = new HashMap<>();
 
     public Recommendation(String username){
-        this.username = username;
-        this.Classics = 100;
-        this.SciFi = 100;
-        this.Horror = 100;
-        this.Thriller = 100;
-        this.Sports = 100;
-        this.Comics = 100;
-        this.Romance = 100;
-        this.Business = 100;
-        this.other = 100;
-        this.SciFiCount = 0;
-        this.HorrorCount = 0;
-        this.ThrillerCount = 0;
-        this.SportsCount = 0;
-        this.ComicsCount = 0;
-        this.RomanceCount = 0;
-        this.BusinessCount = 0;
-        this.otherCount = 0;
+        category.put("Classics", 100.00);
+        category.put("SciFi", 100.00);
+        category.put("Horror", 100.00);
+        category.put("Thriller", 100.00);
+        category.put("Sports", 100.00);
+        category.put("Comics", 100.00);
+        category.put("Romance", 100.00);
+        category.put("Business", 100.00);
+        category.put("Others", 100.00);
+
     }
 
     public Recommendation(){}
 
-
     /**
      * Getter and Setter
      */
-    public String getUsername() {
-        return username;
+
+    public HashMap<String, Double> getCategory() {
+        return category;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public double getClassics() {
-        return Classics;
-    }
-
-    public void setClassics(double classics) {
-        Classics = classics;
-    }
-
-    public double getSciFi() {
-        return SciFi;
-    }
-
-    public void setSciFi(double sciFi) {
-        SciFi = sciFi;
-    }
-
-    public double getHorror() {
-        return Horror;
-    }
-
-    public void setHorror(double horror) {
-        Horror = horror;
-    }
-
-    public double getThriller() {
-        return Thriller;
-    }
-
-    public void setThriller(double thriller) {
-        Thriller = thriller;
-    }
-
-    public double getSports() {
-        return Sports;
-    }
-
-    public void setSports(double sports) {
-        Sports = sports;
-    }
-
-    public double getComics() {
-        return Comics;
-    }
-
-    public void setComics(double comics) {
-        Comics = comics;
-    }
-
-    public double getRomance() {
-        return Romance;
-    }
-
-    public void setRomance(double romance) {
-        Romance = romance;
-    }
-
-    public double getBusiness() {
-        return Business;
-    }
-
-    public void setBusiness(double business) {
-        Business = business;
-    }
-
-    public double getOther() {
-        return other;
-    }
-
-    public void setOther(double other) {
-        this.other = other;
-    }
-
-    public Integer getClassicsCount() {
-        return ClassicsCount;
-    }
-
-    public void setClassicsCount(Integer classicsCount) {
-        ClassicsCount = classicsCount;
-    }
-
-    public Integer getSciFiCount() {
-        return SciFiCount;
-    }
-
-    public void setSciFiCount(Integer sciFiCount) {
-        SciFiCount = sciFiCount;
-    }
-
-    public Integer getHorrorCount() {
-        return HorrorCount;
-    }
-
-    public void setHorrorCount(Integer horrorCount) {
-        HorrorCount = horrorCount;
-    }
-
-    public Integer getThrillerCount() {
-        return ThrillerCount;
-    }
-
-    public void setThrillerCount(Integer thrillerCount) {
-        ThrillerCount = thrillerCount;
-    }
-
-    public Integer getSportsCount() {
-        return SportsCount;
-    }
-
-    public void setSportsCount(Integer sportsCount) {
-        SportsCount = sportsCount;
-    }
-
-    public Integer getComicsCount() {
-        return ComicsCount;
-    }
-
-    public void setComicsCount(Integer comicsCount) {
-        ComicsCount = comicsCount;
-    }
-
-    public Integer getRomanceCount() {
-        return RomanceCount;
-    }
-
-    public void setRomanceCount(Integer romanceCount) {
-        RomanceCount = romanceCount;
-    }
-
-    public Integer getBusinessCount() {
-        return BusinessCount;
-    }
-
-    public void setBusinessCount(Integer businessCount) {
-        BusinessCount = businessCount;
-    }
-
-    public Integer getOtherCount() {
-        return otherCount;
-    }
-
-    public void setOtherCount(Integer otherCount) {
-        this.otherCount = otherCount;
+    public void setCategory(HashMap<String, Double> category) {
+        this.category = category;
     }
 }
